7c683429c3bff3a72f2fe5baf8626cae2f7256eb
wintercodeNKJJ <kenfackjordanjuunior@gmail.com>
2023-04-21 09:35:02 +0100

    font

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const { Products } = Datastc("products", 'en')
   const { Cnews } = Ndatastc('cnews', 'en')
   return (
-    <div className="App myfont">
+    <div className="App font-serif">
       <Router>
         <StateContext>
           <Header />
